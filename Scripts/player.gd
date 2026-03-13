5c45929198f3f3d05126a35fead75024da4fbb45
Max Lima <maxlimasales2019@gmail.com>
2026-03-13 16:00:51 -0300

    Player Movement
    Added input actions and implemented movement in player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+@onready var sprite = $Player
+
+var max_speed = 200
+var acceleration = 800
+var friction = 900
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide()
