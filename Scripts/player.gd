3bcf3bf3fdb40402a34c4da224b30bad9166b2b3
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-14 10:28:44 +0530

    created animations.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@onready var sprite = $Player
+@onready var sprite = $PlayerAnimation
+
 
 var max_speed = 200
 var acceleration = 800
@@ -9,8 +10,10 @@ var friction = 900
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction != Vector2.ZERO:
+		sprite.play("walk")
 		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	else:
+		sprite.play("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
 	move_and_slide()
